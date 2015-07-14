67146145ab13e0042648f4cd13f27f20714a6824
khoai <dungcoivb@gmail.com>
2015-07-14 18:10:05 +0700

    add --raise option

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
-CC = gcc -Wall -o
+# CC = gcc
+CC = gcc -Wall
+DBGCFLAGS = -g -DDEBUG
 RM = rm
+SRCS = udperf.c
+OBJS = $(SRCS:.c=.o)
+EXE = udperf
 
 udperf: udperf.c
-	$(CC) $@ $^
+	$(CC) -o $@ $^
+
+debug: $(SRCS)
+	$(CC) $(DBGCFLAGS) -o $(EXE) $^
 
 clean:
-	$(RM) udperf
+	$(RM) $(EXE)
--- udperf.c
+++ udperf.c
@@ -70,7 +70,7 @@ void receive_bytes(struct Dest *dest, int is_print) {
     /* print the server's reply */
     int n = recvfrom(dest->fd, buf, sizeof (buf), 0, (struct sockaddr *) &dest->serveraddr, &dest->serverlen);
     if (n < 0)
-        error((char*) "ERROR in recvfrom");
+        error("ERROR in recvfrom");
     if (is_print)
         printf("Echo from server: %s\n", buf);
 }
@@ -138,13 +138,14 @@ size_t send_by_bandwitdh(struct Dest *dest, size_t size, int64_t time) {
 
 static void show_help(void) {
     const char content[] = "udperf 0.1 - small tool generate UDP bandwidth for test\n" \
-		"Usage:\n" \
-		" -c, --client <host>           connecting to <host>\n" \
-		" -p, --port <port>             server port to connect to\n" \
+        "Usage:\n" \
+        " -c, --client <host>           connecting to <host>\n" \
+        " -p, --port <port>             server port to connect to\n" \
         " -b, --bandwidth <bandwidth>   test bandwidth (default = '10M')\n" \
         " -t, --time <time>             test time (default = '10' second)\n" \
-		" -h, --help                    show help\n" \
-	;
+        " -r, --raise                   is raise bandwidth time (default not raise)\n" \
+        " -h, --help                    show help\n" \
+    ;
     printf(content);
     exit(EXIT_FAILURE);
 }
@@ -154,6 +155,7 @@ static struct option long_options[] = {
     {"port", required_argument, 0, 'p'},
     {"time", required_argument, 0, 't'},
     {"bandwidth", required_argument, 0, 'b'},
+    {"raise", no_argument, 0, 'r'},
     {"help", no_argument, 0, 'h'},
     {NULL, 0, NULL, 0}
 };
@@ -178,15 +180,60 @@ size_t read_size(const char* sz_size) {
     return size;
 }
 
+void raise_bandwidth(struct Dest *dest, size_t bandwidth) {
+    printf("[-] Raise bandwidth start\n");
+    size_t total_sent = 0;
+    int64_t sleep_time;
+    char size_hrdr[20] = {0}; // size human-readable
+    char time_hrdr[20] = {0}; // time human-readable
+    struct timespec last_time = {0, 0};
+    struct timespec curr_time = {0, 0};
+    struct timespec begin, end = {0, 0};
+    size_t bw = UDP_MSS;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &begin);
+    while (1) {
+        // sleep time
+        sleep_time = SECOND;
+        if (last_time.tv_nsec && last_time.tv_sec) {
+            clock_gettime(CLOCK_MONOTONIC_RAW, &curr_time);
+            sleep_time -= (curr_time.tv_sec - last_time.tv_sec)* 1e6
+                + (curr_time.tv_nsec - last_time.tv_nsec) / 1e3;
+            last_time = curr_time;
+        }
+        if (sleep_time < 0)
+            error("sleep_time is minus");
+        usleep(sleep_time);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &last_time);
+        // raise
+        readable_fs(bw, size_hrdr);
+        printf(" - raise: %s\n", size_hrdr);
+        size_t nsent = send_by_bandwitdh(dest, bw, SECOND * 0.9);
+        total_sent += nsent;
+        if (bw == bandwidth)
+            break;
+        bw *= 2;
+        if (bw > bandwidth)
+            bw = bandwidth;
+    }
+    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+    int64_t time_span = (end.tv_sec - begin.tv_sec) * 1e6
+        + (end.tv_nsec - begin.tv_nsec) / 1e3;
+    readable_fs(total_sent, size_hrdr);
+    readable_time(time_span, time_hrdr);
+    printf(" - total_sent=%s/%s\n", size_hrdr, time_hrdr);
+    printf("[-] Raise bandwidth done\n");
+}
+
 int main(int argc, char** argv) {
     char *host_ = NULL;
     int port_ = 0;
     int time_ = 10;
     size_t bandwidth_ = 10 * 1024 * 1024;
+    char is_raise_ = 0;
 
     int c;
     int option_index = 0;
-    while ((c = getopt_long(argc, argv, "p:c:t:b:h:",
+    while ((c = getopt_long(argc, argv, "p:c:t:b:rh:",
         long_options, &option_index)) != -1) {
         switch (c) {
             case 'c':
@@ -202,6 +249,9 @@ int main(int argc, char** argv) {
                 break;
             case 'b':
                 bandwidth_ = read_size((const char*) optarg);
+                break;
+            case 'r':
+                is_raise_ = 1;
                 break;
             case 'h':
             default:
@@ -220,28 +270,35 @@ int main(int argc, char** argv) {
     char size_hrdr[20] = {0}; // size human-readable
     char time_hrdr[20] = {0}; // time human-readable
     readable_fs(bandwidth_, size_hrdr);
-    printf("[-] Config: host=%s:%d, bandwidth=%s, time=%ds\n", host_, port_, size_hrdr, time_);
+    printf("[-] Config: host=%s:%d, bandwidth=%s, time=%ds, is_raise=%d\n",
+        host_, port_, size_hrdr, time_, is_raise_);
     struct Dest dest;
     init_socket(&dest, host_, port_);
 
+    // raise bandwidth
+    if (is_raise_)
+        raise_bandwidth(&dest, bandwidth_);
+
+    // generate bandwidth
+    printf("[-] Bandwidth test\n");
     size_t total_sent = 0;
-    int sleep_time = SECOND;
-    struct timespec lastTime = {0, 0};
-    struct timespec currentTime = {0, 0};
+    int64_t sleep_time = SECOND;
+    struct timespec last_time = {0, 0};
+    struct timespec curr_time = {0, 0};
     struct timespec begin, end = {0, 0};
     clock_gettime(CLOCK_MONOTONIC_RAW, &begin);
     while (1) {
         sleep_time = SECOND;
-        if (lastTime.tv_nsec && lastTime.tv_sec) {
-            clock_gettime(CLOCK_MONOTONIC_RAW, &currentTime);
-            sleep_time -= (currentTime.tv_sec - lastTime.tv_sec)* 1e6
-                + (currentTime.tv_nsec - lastTime.tv_nsec) / 1e3;
-            lastTime = currentTime;
+        if (last_time.tv_nsec && last_time.tv_sec) {
+            clock_gettime(CLOCK_MONOTONIC_RAW, &curr_time);
+            sleep_time -= (curr_time.tv_sec - last_time.tv_sec)* 1e6
+                + (curr_time.tv_nsec - last_time.tv_nsec) / 1e3;
+            last_time = curr_time;
         }
         if (sleep_time < 0)
             error("sleep_time is minus");
         usleep(sleep_time);
-        clock_gettime(CLOCK_MONOTONIC_RAW, &lastTime);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &last_time);
         size_t nsent = send_by_bandwitdh(&dest, bandwidth_, SECOND * 0.9);
         total_sent += nsent;
         readable_fs(nsent, size_hrdr);
